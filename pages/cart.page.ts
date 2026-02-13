bf2d989b69963e4aa2a0e51cef7fe28d97b70119
Nguyen Chung Van <ncvan2301@gmail.com>
2026-02-13 20:07:37 +0700

    fix: CI fail when run TC11

--- pages/cart.page.ts
+++ pages/cart.page.ts
@@ -4,7 +4,7 @@ export class CartPage {
     constructor(private page: Page) { }
 
     async goToCart() {
-        await this.page.getByRole('link', { name: 'Cart', exact: true }).click();
+        await this.page.locator('.shop-menu a[href="/view_cart"]').click();
     }
 
     async viewCart() {
